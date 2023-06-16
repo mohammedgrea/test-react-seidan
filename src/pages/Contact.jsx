24e3c4b4906457a53a7f6f85a763bc8c3208f830
Mohammed GREA <mohammed.grea@uit.ac.ma>
2023-06-16 19:03:26 +0100

    add contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,47 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../styles/contact.css";
 export default function Contact() {
-  return <div>Contact</div>;
+  const [email, setEamil] = useState("");
+  const [username, setUsername] = useState("");
+  const [massage, setMessage] = useState("");
+
+  function Handelsubmit(e) {
+    e.preventDefault();
+    setEamil("");
+    setMessage("");
+    setUsername("");
+  }
+  return (
+    <div className="contact-container">
+      <h1>contact us</h1>
+      <form className="form-container" onSubmit={Handelsubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEamil(e.target.value)}
+        />
+        <textarea
+          placeholder="You message"
+          rows="4"
+          cols="50"
+          value={massage}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button className="btn-submit" type="sibmit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
